775239862c790e03d0256af92818169913d890d3
Rodrigo Carmonaa <rcarmona2023@alu.uct.cl>
2023-11-26 23:28:35 -0300

    Update visual.py

--- visual.py
+++ visual.py
@@ -1,6 +1,8 @@
 import PIL
 import tkinter as tk
 from PIL import Image, ImageTk
+import random
+
 
 class organismo:
     def __init__(self, posicion, vida, energia, velocidad):
@@ -9,62 +11,166 @@ class organismo:
         self.energia = energia
         self.velocidad = velocidad
         
-    def mover(self):
-        nueva_posicion = self.posicion + self.velocidad
-        self.posicion = nueva_posicion
+class Planta(organismo):
+    def __init__(self, posicion, tipo, vida, energia, velocidad, ciclo_vida, tiempo_sin_agua):
+        super().__init__(posicion, vida, energia, velocidad)
+        self.tipo = tipo
+        self.ciclo_vida = ciclo_vida  # Duración del ciclo de vida en iteraciones
+        self.tiempo_sin_agua = tiempo_sin_agua  # Número de iteraciones sin agua antes de secarse
 
-    
-    def comer(self):
-        energia = self.energia + 1
-        self.energia = energia
-
-    
-    def reproducir(self):
-        return self
-        
+    def crecer(self, cantidad_agua):
+        # La planta crece en función de la cantidad de agua que recibe
+        factor_crecimiento = cantidad_agua / self.tiempo_sin_agua
+        self.vida += factor_crecimiento
+        self.energia += factor_crecimiento
 
     def morir(self):
-        vida = self.vida - 1
-        self.vida = vida
-        
+        # La planta muere cuando su ciclo de vida llega a cero o si no recibe agua durante un tiempo
+        self.vida = max(0, self.vida - 1)
+        self.energia = max(0, self.energia - 1)
 
-class Leon(organismo):
-    def __init__(self, posicion, vida, energia, velocidad, nombre, especie, dieta):
+    def ser_consumida(self):
+        # La planta es consumida por un animal, disminuyendo su vida y energía
+        if self.vida > 0:
+            self.vida -= 1
+            self.energia -= 1
+
+    def actualizar_estado(self, cantidad_agua):
+        # Actualiza el estado de la planta en cada iteración
+        self.crecer(cantidad_agua)
+        self.morir()
+
+    def reproducirse(self):
+        # Lógica de reproducción de la planta
+        nueva_planta = Planta(f"NuevaPlanta_{random.randint(1, 100)}", self.tipo, self.ubicacion, vida=1, energia=1, velocidad=1, ciclo_vida=self.ciclo_vida, tiempo_sin_agua=self.tiempo_sin_agua)
+        return nueva_planta
+
+#####################################################################
+#                           ANIMAL
+#####################################################################
+class Animal(organismo):
+    def __init__(self, posicion, especie, vida, energia, velocidad, hambre, sed, ciclo_vida):
         super().__init__(posicion, vida, energia, velocidad)
+        self.especie = especie
+        self.hambre = hambre
+        self.sed = sed
+        self.campo_vision = 2  # Campo de visión alrededor del animal
+        self.ciclo_vida = ciclo_vida  # Número de iteraciones antes de morir sin comida o agua
+        self.tiempo_sin_comida = 0
+        self.tiempo_sin_agua = 0
+
+    def moverse(self, objetivo=None):
+        if objetivo:
+            # Calcula la dirección hacia el objetivo y mueve el animal en esa dirección
+            direccion_x = objetivo.ubicacion[0] - self.ubicacion[0]
+            direccion_y = objetivo.ubicacion[1] - self.ubicacion[1]
+            distancia = max(abs(direccion_x), abs(direccion_y))
+
+            if distancia > 0:
+                direccion_x /= distancia
+                direccion_y /= distancia
+
+                # Ajusta la velocidad del depredador al encontrar a la presa
+                if isinstance(objetivo, Planta):
+                    velocidad = min(self.velocidad, objetivo.velocidad)
+                else:
+                    velocidad = min(self.velocidad * 2, objetivo.velocidad)
+
+                # Mueve el animal en la dirección ajustada por su velocidad
+                nueva_ubicacion = (
+                    self.ubicacion[0] + int(direccion_x * velocidad),
+                    self.ubicacion[1] + int(direccion_y * velocidad)
+                )
+                self.ubicacion = nueva_ubicacion
+        else:
+            # Se mueve a una nueva ubicación dentro de su campo de visión
+            super().moverse()
+
+    def buscar_presa(self, presas):
+        # Busca presas dentro de su campo de visión
+        for presa in presas:
+            distancia = abs(self.ubicacion[0] - presa.ubicacion[0]) + abs(self.ubicacion[1] - presa.ubicacion[1])
+            if distancia <= self.campo_vision and self.hambre > 0:
+                return presa
+        return None
+
+    def buscar_charco(self, charcos):
+        # Busca charcos de agua dentro de su campo de visión
+        for charco in charcos:
+            distancia = abs(self.ubicacion[0] - charco.ubicacion[0]) + abs(self.ubicacion[1] - charco.ubicacion[1])
+            if distancia <= self.campo_vision and self.sed > 0:
+                return charco
+        return None
+
+    def alimentarse(self, presa):
+        # Se alimenta de la presa
+        if presa:
+            self.energia += presa.vida
+            self.hambre = max(0, self.hambre - presa.vida)
+
+    def beber(self, charco):
+        # Bebe agua del charco
+        if charco:
+            self.energia += charco.agua
+            self.sed = max(0, self.sed - charco.agua)
+
+    def reproducirse(self):
+        # Lógica de reproducción
+        nuevo_animal = Animal(f"NuevoAnimal_{random.randint(1, 100)}", self.especie, self.ubicacion, vida=1, energia=1, velocidad=1, hambre=1, sed=1)
+        return nuevo_animal
+
+class Depredador(Animal):
+    def __init__(self, posicion, vida, energia, velocidad, hambre, sed, ciclo_vida):
+        super().__init__(posicion, vida, energia, velocidad, hambre, sed, ciclo_vida)
+
+    def cazar(self, presas):
+        # Lógica de caza: El depredador busca presas y las persigue
+        presa = self.buscar_presa(presas)
+        self.moverse(presa)
+        self.alimentarse(presa)
+
+    def ciclo_vida(self):
+        # Añade lógica para el ciclo de vida de los depredadores, por ejemplo, decrementa su vida y energía con el tiempo
+        self.vida -= 1
+        self.energia -= 1
+
+
+class Presa(Animal):
+    def __init__(self, posicion, vida, energia, velocidad , hambre, sed, ciclo_vida):
+        super().__init__(posicion, vida, energia, velocidad, hambre, sed, ciclo_vida)
+
+    def huir(self, depredadores):
+        # Lógica de huida: La presa busca depredadores y trata de huir
+        depredador = self.buscar_depredador(depredadores)
+        self.moverse(depredador)
+
+    def ciclo_vida(self):
+        # Añade lógica para el ciclo de vida de las presas, por ejemplo, decrementa su vida y energía con el tiempo
+        self.vida -= 1
+        self.energia -= 1
+
+
+########################################################################################################
+
+class Leon(Depredador):
+    def __init__(self, posicion, vida, energia, velocidad, nombre, especie, dieta):
+        super().__init__(posicion, vida, energia, velocidad, hambre=1, sed=1, ciclo_vida=100)
         self.nombre = nombre
         self.especie = especie
         self.dieta = dieta
         self.velocidad = 20
         self.vida = 100
 
-    def cazar(self):
-        self.energia -= 20
-        self.velocidad += 8
-
-    def comer(self):
-        self.energia += 30
-
-    def dormir(self):
-        self.energia += 50
-
-class cebras(organismo):
-    def __init__(self, posicion, vida, energia, velocidad, nombre, especie, dieta):
-        super().__init__(posicion, vida, energia, velocidad)
-        self.nombre = nombre
-        self.especie = especie
-        self.dieta = dieta
-        self.velocidad = 15
-        self.vida = 100
-
-    def huir(self):
-        self.energia -= 20
-        self.velocidad += 10
-
-    def comer(self):
-        self.energia += 30
+    def cazar(self, presa):
+        # La función de caza específica para el león, disminuye la vida y aumenta la energía
+        if isinstance(presa, Presa):
+            self.energia += presa.vida
+            presa.vida = max(0, presa.vida - 30)  # Asumiendo que la presa pierde 30 de vida al ser cazada
+            self.vida = min(100, self.vida + 10)  # El león gana 10 de vida al cazar
 
     def dormir(self):
         self.energia += 50
+
         
 class jirafa(organismo):
     def __init__(self, posicion, vida, energia, velocidad, nombre, especie, dieta):
@@ -345,9 +451,11 @@ class Ventana(tk.Tk):
         self.filas = filas
         self.columnas = columnas
         self.ancho_celda = ancho_celda
-        self.lion_image = Image.open("leon.png")  # Cargar la imagen como un atributo de la clase
+        self.lion_image = Image.open("leon.png")
+        self.leon_posicion = [5, 8]  # Posición inicial del león
         self.crear_cuadricula()
         self.mostrar_leon()
+        self.mover_leon()  # Inicia el movimiento del león
 
     def crear_cuadricula(self):
         canvas = tk.Canvas(self, width=self.columnas * self.ancho_celda, height=self.filas * self.ancho_celda)
@@ -379,6 +487,20 @@ class Ventana(tk.Tk):
         # Mostrar la imagen del león en el lienzo
         self.canvas.create_image(x_posicion, y_posicion, anchor=tk.NW, image=lion_image)
 
+    def mover_leon(self):
+        # Actualiza la posición del león
+        self.leon_posicion[0] += 1  # Mueve el león hacia la derecha
+        # Si el león sale de los límites, vuelve a la posición inicial
+        if self.leon_posicion[0] >= self.columnas:
+            self.leon_posicion[0] = 0
+
+        # Limpia el canvas antes de volver a dibujar al león en su nueva posición
+        self.canvas.delete("all")
+        self.crear_cuadricula()
+        self.mostrar_leon()
+
+        # Llama a esta función nuevamente después de un breve período (por ejemplo, 100 milisegundos)
+        self.after(100, self.mover_leon)
 if __name__ == "__main__":
     ventana = Ventana(filas=15, columnas=15, ancho_celda=40)
     ventana.mainloop()
